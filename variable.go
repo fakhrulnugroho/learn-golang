e91f1e72099ef35274a5bb2458a640e42377092a
fakhrulnugroho <fakhrl.fanani.nugroho@gmail.com>
2022-04-13 04:39:15 +0700

    5: add variable

---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string;
+	name = "Fakhrul Fanani";
+	fmt.Println(name)
+	name = "Fakhrul Nugroho"
+	fmt.Println(name)
+
+	var lastName = "Nugroho"
+	fmt.Println(lastName)
+
+	var age int8 = 19
+	fmt.Println(age)
+
+	country := "Indonesia";
+	fmt.Println(country)
+}
