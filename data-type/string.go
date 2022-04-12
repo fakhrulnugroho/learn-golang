579304f3852c4d3794c7e0e5884752c9b36d5d01
fakhrulnugroho <fakhrl.fanani.nugroho@gmail.com>
2022-04-13 04:21:16 +0700

    4: add string data type

---
+++ data-type/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Fakhrul")
+	fmt.Println("Fakhrul Fanani")
+	fmt.Println("Fakhrul Fanani Nugroho")
+
+	fmt.Println(len("Fakhrul"))
+	fmt.Println("Fakhrul Fanani"[0])
+	fmt.Println("Fakhrul Fanani Nugroho"[1])
+}
